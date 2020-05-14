e485e84f85a090a7873bbb701d39a066e18939b0
ayb <first.youbix@gmail.com>
2020-05-14 11:50:47 +0000

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -11,19 +11,20 @@ class Home extends Component{
         <div className="App">
           <Header />
           <div >
-              <NavLink to="/Rooms">
-                <button className='btn'><h2 className="titleroom">Rooms</h2> <Avatar size={90} style={{ color: '#ffffff' , background:'none' }}icon={<HomeOutlined />}/></button>
-                {/* <Card title="Rooms" bordered={true} style={{ width: 300 }}>
-                    <img className="img" src={chooseRoom}></img>
-                </Card> */}
-              </NavLink>
-            
-              <NavLink to="/Setting">
-              <button className='btn'><h2 className="titleroom">Setting</h2> <Avatar size={90} style={{ color: '#ffffff' , background:'none' }}icon={<SettingOutlined />}/></button>
-                {/* <Card title="Setting" bordered={true} style={{ width: 300 }}>
-                    <img className="img" src={param}></img>
-                </Card> */}
-              </NavLink>
+
+            <NavLink to="/Rooms">
+              <button className='btn'><h2 className="titleroom">Rooms</h2> <Avatar size={90} style={{ color: '#ffffff' , background:'none' }}icon={<HomeOutlined />}/></button>
+              {/* <Card title="Rooms" bordered={true} style={{ width: 300 }}>
+                  <img className="img" src={chooseRoom}></img>
+              </Card> */}
+            </NavLink>
+          
+            <NavLink to="/Setting">
+            <button className='btn'><h2 className="titleroom">Setting</h2> <Avatar size={90} style={{ color: '#ffffff' , background:'none' }}icon={<SettingOutlined />}/></button>
+              {/* <Card title="Setting" bordered={true} style={{ width: 300 }}>
+                  <img className="img" src={param}></img>
+              </Card> */}
+            </NavLink>
             
           </div>
             
