fde475604fcbe3724ed9801ac4478d4f04fff564
oumayma essemrhouni <42737226+ouma77@users.noreply.github.com>
2020-05-15 00:23:31 +0200

    change home style

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,30 +6,34 @@ import '../App.css';
 import Header from './Header.js';
 
 class Home extends Component{
-    render(){
-      return(
-        <div className="App">
-          <Header />
-          <div >
-              <NavLink to="/Rooms">
-                <button className='btn'><h2 className="titleroom">Rooms</h2> <Avatar size={90} style={{ color: '#ffffff' , background:'none' }}icon={<HomeOutlined />}/></button>
-                {/* <Card title="Rooms" bordered={true} style={{ width: 300 }}>
-                    <img className="img" src={chooseRoom}></img>
-                </Card> */}
-              </NavLink>
-            
-              <NavLink to="/Setting">
-              <button className='btn'><h2 className="titleroom">Setting</h2> <Avatar size={90} style={{ color: '#ffffff' , background:'none' }}icon={<SettingOutlined />}/></button>
-                {/* <Card title="Setting" bordered={true} style={{ width: 300 }}>
-                    <img className="img" src={param}></img>
-                </Card> */}
-              </NavLink>
-            
+  render(){
+    return(
+      <div className="App">
+        <Header />
+        <div className="home">
+          <div className="hometext">
+             <h1>SmartHome-App</h1>
+             <p>Contrôlez votre maison depuis l’application SmartHome-App sur votre téléphone mobile.</p>
+             <p>Découvrez plus de façons de contrôler votre maison :</p>
           </div>
-            
+
+            <NavLink to="/Rooms">
+              <button className='btn'><h2 className="titleroom">Rooms</h2> 
+              <Avatar size={50} style={{ color: '#007bff' , background:'none' }}icon={<HomeOutlined />}/>
+              </button>
+            </NavLink>
+          
+            <NavLink to="/Setting">
+            <button className='btn'><h2 className="titleroom">Setting</h2> 
+            <Avatar size={50} style={{ color: '#007bff' , background:'none' }}icon={<SettingOutlined />}/>
+            </button>
+            </NavLink>
+          
         </div>
-      );
-    }
+          
+      </div>
+    );
   }
+}
   
 export default Home;
