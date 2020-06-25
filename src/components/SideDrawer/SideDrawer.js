e4098c9162d9530fc69bedc9f0f8294c2f437370
ayb <first.youbix@gmail.com>
2020-06-25 22:18:51 +0100

    sideDrawer family

--- src/components/SideDrawer/SideDrawer.js
+++ src/components/SideDrawer/SideDrawer.js
@@ -1,26 +1,20 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './SideDrawer.css';
-import { HomeOutlined,SettingOutlined,UserOutlined,LogoutOutlined,InfoCircleOutlined} from '@ant-design/icons';
+import { HomeOutlined,SettingOutlined,LogoutOutlined,InfoCircleOutlined} from '@ant-design/icons';
 
 
 const sideDrawer = props => { 
 let drawerClasses = 'side-drawer';
-let type = null
-
 if(props.show) {
     drawerClasses = 'side-drawer open';
 }
-fetch('/session')
-    .then(res=>res.json())
-    .then(data=>{ type=data.type })
 
     return (
 <nav className={drawerClasses}>
     <ul>
         <li><NavLink to="/Rooms"><HomeOutlined/> Rooms</NavLink></li>
         <li><NavLink to="/Setting"><SettingOutlined/> General</NavLink></li>
-        {type==='admin'?<li><NavLink to="/Profil"><UserOutlined/> Family</NavLink></li>:null}
         <li><NavLink to="/About"><InfoCircleOutlined/> About us</NavLink></li>
         <li><NavLink onClick={()=>fetch('/disconnect')} exact to="/"><LogoutOutlined/> Disconnect</NavLink></li> 
     </ul>
--- src/components/SideDrawer/SideDrawerHome.js
+++ src/components/SideDrawer/SideDrawerHome.js
@@ -6,20 +6,13 @@ import {UserOutlined,LogoutOutlined,InfoCircleOutlined} from '@ant-design/icons'
 
 const sideDrawerHome = props => { 
 let drawerClasses = 'side-drawer';
-let type = null
-
 if(props.show) {
     drawerClasses = 'side-drawer open';
 }
-fetch('/session')
-    .then(res=>res.json())
-    .then(data=>{ type=data.type })
-
 
     return (
 <nav className={drawerClasses}>
     <ul>
-        {type==='admin'?<li><NavLink to="/Profil"><UserOutlined/> Family</NavLink></li>:null}
         <li><NavLink to="/About"><InfoCircleOutlined/> About us</NavLink></li>
         <li><NavLink onClick={()=>fetch('/disconnect')} exact to="/"><LogoutOutlined/> Disconnect</NavLink></li> 
     </ul>
